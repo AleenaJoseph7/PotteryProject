5d42dc55e8f7dc0d5f4f8d403b874441ffbac65f
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-11-13 10:01:14 +0530

    Cart Page set input fields and cartdb for it and registerd it with admin

--- Myapp/admin.py
+++ Myapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from Myapp.models import catergorydb,productdb,potterydb
-from webapp.models import Orderdb,Bookingdb
+from webapp.models import Orderdb,Bookingdb,Cartdb
 # Register your models here.
 admin.site.register(catergorydb)
 admin.site.register(productdb)
 admin.site.register(potterydb)
 admin.site.register(Orderdb)
 admin.site.register(Bookingdb)
+admin.site.register(Cartdb)
