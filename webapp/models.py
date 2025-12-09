74e75d515a70138fb7d35441f70028696834a0ee
AleenaJoseph7 <aleenajoseph18122002@gmail.com>
2025-12-09 18:24:04 +0530

    alignment fix

--- webapp/models.py
+++ webapp/models.py
@@ -1,56 +1,61 @@
 from django.db import models
 
+
 # Create your models here.
 class Signupdb(models.Model):
-    Signup_username=models.CharField(max_length=30,null=True,blank=True)
-    Signup_email=models.EmailField(null=True,blank=True)
-    Signup_mobile=models.IntegerField(null=True,blank=True)
-    Signup_password=models.CharField(max_length=30,null=True,blank=True)
-    Signup_confirm=models.CharField(max_length=30,null=True,blank=True)
+    Signup_username = models.CharField(max_length=30, null=True, blank=True)
+    Signup_email = models.EmailField(null=True, blank=True)
+    Signup_mobile = models.IntegerField(null=True, blank=True)
+    Signup_password = models.CharField(max_length=30, null=True, blank=True)
+    Signup_confirm = models.CharField(max_length=30, null=True, blank=True)
+
 
 class Bookingdb(models.Model):
-    Booking_firstname=models.CharField(max_length=30,null=True,blank=True)
-    Booking_lastname=models.CharField(max_length=30,null=True,blank=True)
-    Booking_age=models.IntegerField(null=True,blank=True)
-    Booking_gender=models.CharField(max_length=30,null=True,blank=True)
-    Booking_address=models.TextField(null=True,blank=True)
-    Booking_email=models.EmailField(null=True,blank=True)
-    Booking_mobile=models.IntegerField(null=True,blank=True)
-    Booking_class=models.CharField(max_length=30,null=True,blank=True)
+    Booking_firstname = models.CharField(max_length=30, null=True, blank=True)
+    Booking_lastname = models.CharField(max_length=30, null=True, blank=True)
+    Booking_age = models.IntegerField(null=True, blank=True)
+    Booking_gender = models.CharField(max_length=30, null=True, blank=True)
+    Booking_address = models.TextField(null=True, blank=True)
+    Booking_email = models.EmailField(null=True, blank=True)
+    Booking_mobile = models.IntegerField(null=True, blank=True)
+    Booking_class = models.CharField(max_length=30, null=True, blank=True)
 
     def __str__(self):
         return self.Booking_firstname
 
+
 class Orderdb(models.Model):
-    Order_fullname=models.CharField(max_length=30,null=True,blank=True)
-    Order_email=models.EmailField(null=True,blank=True)
-    Order_phone=models.IntegerField(null=True,blank=True)
-    Order_address=models.TextField(null=True,blank=True)
-    Order_payment=models.CharField(max_length=30,null=True,blank=True)
+    Order_fullname = models.CharField(max_length=30, null=True, blank=True)
+    Order_email = models.EmailField(null=True, blank=True)
+    Order_phone = models.IntegerField(null=True, blank=True)
+    Order_address = models.TextField(null=True, blank=True)
+    Order_payment = models.CharField(max_length=30, null=True, blank=True)
 
     def __str__(self):
         return self.Order_fullname
 
+
 class Cartdb(models.Model):
-    Singlepottery_username=models.CharField(max_length=30,null=True,blank=True)
-    Singlepottery_name=models.CharField(max_length=30,null=True,blank=True)
-    Singlepottery_price=models.IntegerField(null=True,blank=True)
+    Singlepottery_username = models.CharField(max_length=30, null=True, blank=True)
+    Singlepottery_name = models.CharField(max_length=30, null=True, blank=True)
+    Singlepottery_price = models.IntegerField(null=True, blank=True)
     Singlepottery_total = models.IntegerField(null=True, blank=True)
     Singlepottery_quantity = models.CharField(max_length=30, null=True, blank=True)
     Singlepottery_image = models.ImageField(upload_to="Cart Images", null=True, blank=True)
-    Potteryid=models.IntegerField(null=True,blank=True)
+    Potteryid = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.Singlepottery_name
 
+
 class Checkoutdb(models.Model):
-    Fullame=models.CharField(max_length=30,null=True,blank=True)
-    Email=models.EmailField(max_length=30,null=True,blank=True)
-    Address=models.CharField(max_length=30,null=True,blank=True)
-    City=models.CharField(max_length=30,null=True,blank=True)
-    Pincode=models.IntegerField(null=True,blank=True)
-    Total_amount=models.IntegerField(null=True,blank=True)
-    Checkout_Username=models.CharField(max_length=30,null=True,blank=True)
+    Fullame = models.CharField(max_length=30, null=True, blank=True)
+    Email = models.EmailField(max_length=30, null=True, blank=True)
+    Address = models.CharField(max_length=30, null=True, blank=True)
+    City = models.CharField(max_length=30, null=True, blank=True)
+    Pincode = models.IntegerField(null=True, blank=True)
+    Total_amount = models.IntegerField(null=True, blank=True)
+    Checkout_Username = models.CharField(max_length=30, null=True, blank=True)
 
     def __str__(self):
         return self.Fullame
--- webapp/views.py
+++ webapp/views.py
@@ -178,25 +178,25 @@ def PaymentPage(request):
     discount = round(subtotal * 0.10)
     totalprice = (subtotal + gst + delivery) - discount
 
-        # address fetch
+    # address fetch
     checkout_address = Checkoutdb.objects.filter(Checkout_Username=request.session['username']).first()
     cart_count = Cartdb.objects.filter(Singlepottery_username=request.session['username']).count()
 
-    #PAYMENT DATA FETCHED
-    customer=Checkoutdb.objects.order_by("-id").first()
-    pay=customer.Total_amount
-    amount=int(pay*100)
-    pay_str=str(amount)
+    # PAYMENT DATA FETCHED
+    customer = Checkoutdb.objects.order_by("-id").first()
+    pay = customer.Total_amount
+    amount = int(pay * 100)
+    pay_str = str(amount)
 
     return render(request, "Paymentpage.html", {'product': product,
-                                                    'cart_count': cart_count,
-                                                    'subtotal': subtotal,
-                                                    'discount': discount,
-                                                    'delivery': delivery,
-                                                    'totalprice': totalprice,
-                                                    'gst': gst,
-                                                    'checkout_address': checkout_address
-                                                    })
+                                                'cart_count': cart_count,
+                                                'subtotal': subtotal,
+                                                'discount': discount,
+                                                'delivery': delivery,
+                                                'totalprice': totalprice,
+                                                'gst': gst,
+                                                'checkout_address': checkout_address
+                                                })
 
 
 def savebooking(request):
